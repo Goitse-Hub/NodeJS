0d1dfaebc8a723bf416d0b3387ec7345a41947a4
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-03-18 21:35:14 +0200

    Update index.js

--- Node 10 EventsEmitter - FastFood/index.js
+++ Node 10 EventsEmitter - FastFood/index.js
@@ -25,30 +25,4 @@ fastfood.displayOrderNumber()
 
 
 
-// // Import A Build In Events Module
-// const EventEmitter = require("node:events");
-
-// // Instatiate the Class
-
-// const emitter = new EventEmitter()
-
-// // (2) To Respond To "order-burger" event, register a listener using - on - method
-
-// emitter.on("order-burger", (sideSize, drink) => {
-//     console.log(`Order received! preparing your Burger with ${sideSize} fries and ${drink} `)
-// })
-
-// // you can register multiple listener for the same event
-// //e.g
-
-// emitter.on("order-burger", (sideSize) => {
-//     if (sideSize === "large") {
-//         console.log("Serving fries with special sauce")
-//     }
-// })
-
-// // (1) Emitting An Event using Emit Method
-
-// emitter.emit("order-burger", "large", "Orange juice")
-
 
