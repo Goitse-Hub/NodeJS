dd754f887ff9eef157293e405d9e192de7f9b4c9
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-03-22 21:02:07 +0200

    Update add.js

--- Node 01/add.js
+++ Node 01/add.js
@@ -7,14 +7,3 @@ const add = (a, b) => {
 
 module.exports = add;
 
-/* Instead Of This We Will Use Modules Exports above
-
-// Call The Function
-const sum = add(2, 5);
-
-// Diplay
-console.log(sum);
-
-
-
-*/
