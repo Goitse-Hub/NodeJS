66653dcdd19dc22efef1de6e837ccf33cc313ca5
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-03-23 21:12:07 +0200

    Update index.js

--- Node 08 Call Back Pattern/index.js
+++ Node 08 Call Back Pattern/index.js
@@ -9,14 +9,3 @@ function greetDev(greetFn) {
 
 greetDev(greet);
 
-// function HigherOrderFunction(CallBackFunction) {
-//     const name = "Que";
-//     CallBackFunction(name);
-// }
-
-// HigherOrderFunction(greet);
-
-
-// Synchronous Callback - is a callback which is executed immediately
-
-// Asynchronous Callback - is used to continue or resume code execution after a asynchronous operation is completed
