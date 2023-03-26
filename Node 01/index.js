0e7245d23d44d3635b9f8f326974b6ddf95aeee1
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-03-26 23:32:11 +0200

    Update index.js

--- Node 01/index.js
+++ Node 01/index.js
@@ -1,13 +1,11 @@
 const add = require('./add.js');
 
-//Prompt User For Inputs
 const prompt = require("prompt-sync")();
 
 const inputOne = parseInt(prompt("Enter First Number? "));
 
 const inputTwo = parseInt(prompt("Enter Second Number? "));
 
-// Call The Function To Perform Addition
 const sum = add(inputOne, inputTwo);
 
 console.log(sum);
